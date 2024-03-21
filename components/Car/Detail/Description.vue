aa5f3e6e31966f8f70870cb1d7e0032f0ef169fe
farzane <farzane.madani@gmail.com>
2024-03-21 13:53:19 +0330

    add car detail page

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="mt-5">
+        <div class="mb-10">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum culpa vitae consectetur eos commodi, repellendus quod id libero hic voluptates laboriosam ad, illum quisquam est nemo tempore dolore dolor exercitationem.
+        </div>
+    </div>
+</template>
+<script></script>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,5 +1,11 @@
 <template>
 <div>
     <Navbar></Navbar>
+    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-0 pb-16 w-4/5">
+      <CarDetailHero />
+      <CarDetailAttributes />
+      <CarDetailDescription />
+      <CarDetailContact />
+    </div>
 </div>
 </template>
