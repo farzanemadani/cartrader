a22d5fa1bf6fc5461fdc9a793876e4c94b92c4a5
farzane <farzane.madani@gmail.com>
2024-03-24 19:12:28 +0330

    add custom layout

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,19 +1,19 @@
 <template>
-<div>
-    <Navbar></Navbar>
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-0 pb-16 w-4/5">
-      <CarDetailHero />
-      <CarDetailAttributes />
-      <CarDetailDescription />
-      <CarDetailContact />
-    </div>
-</div>
+  <div>
+    <NuxtLayout name="custom">
+        <CarDetailHero />
+        <CarDetailAttributes />
+        <CarDetailDescription />
+        <CarDetailContact />
+    </NuxtLayout>
+  </div>
 </template>
 <script setup>
- const route = useRoute();
- useHead({
+const route = useRoute();
+useHead({
   title: capitalizeFirstLetter(route.params.name),
- })
+});
+
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-0 pb-16 w-4/5">
+    <NuxtLayout name="custom">
       <div class="mt-32 flex">
         <CarSideBar/>
         <NuxtPage/>
         <!-- <CarCards/> -->
       </div>
-    </div>
+    </NuxtLayout>
 </template>
 <script setup>
 
